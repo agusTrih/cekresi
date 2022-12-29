d51f6c86487f04af1a1634054cd50fcb5eff3c1d
Agustrihanton <agustrihanton97@gmail.com>
2022-12-30 02:30:55 +0700

    fix: setup react

---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import ErrorPage from './pages/error-page';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+  },
+]);
+export default router;
